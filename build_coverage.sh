e3e6ac3bcd731764235e813fa937bbeed2ee5211
zhuyang <1029893460@qq.com>
2022-12-01 23:08:49 +0800

    test7

--- build_coverage.sh
+++ build_coverage.sh
@@ -5,7 +5,7 @@ rm -rf *
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS=--coverage -DCMAKE_C_FLAGS=--coverage ..
 make -j `cat /proc/cpuinfo | grep processor| wc -l`
 ctest
-pip install --user cpp-coveralls
+source ~/.bashrc
 coveralls -r .. --gcov-options '\-lp' -t PAfc6k2qXkZzTkTKfkFUQWkzxGHI7qMem -e tests -e build/CMakeFiles
 
 #test1
